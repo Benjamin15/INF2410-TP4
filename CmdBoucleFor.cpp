6b3479a23e44ecbaa9c7d8729bc4bc43b503a51a
Benjamin15 <benjamin150594@hotmail.fr>
2017-03-17 13:28:44 -0400

    cmdBoucleFor fini non testé
    Code généré pour la class cmdBoucleFor.
    Le code n'a pas encore testé, donc non valide.
    Et remise en forme des commentaires. Les logs connaissent pas la doc,
    même en log2410..

--- CmdBoucleFor.cpp
+++ CmdBoucleFor.cpp
@@ -7,32 +7,56 @@
 
 #include "CmdBoucleFor.h"
 
-// Executer m_nbRepetitions fois les commandes enfant
+/**
+*
+*
+*Executer m_nbRepetitions fois les commandes enfant
+*/
 void CmdBoucleFor::executer(Robot & robot)
 {
-	// A completer
+	for (int i = 0; i < m_nbRepetitions; i++)
+	{
+		m_commandes[i]->executer(robot);
+	}
 }
 
-// Recuperer le nombre de commandes enfant
+/**
+*
+*
+*Recuperer le nombre de commandes enfant
+*/
 int CmdBoucleFor::getNumberChildren(void) const
 { 
-	return 0; // A completer
+	return m_commandes.size();
 }
 
-// Recuperer l'enfant iChild
+/**
+*
+*
+* Recuperer l'enfant iChild
+*/
 const CmdAbs * CmdBoucleFor::getChild(int iChild) const 
 { 
-	return nullptr; // A completer
+	return m_commandes[iChild].get();
 }
 
-// Recuperer l'enfant iChild
+/**
+*
+*
+*
+* Recuperer l'enfant iChild
+*/
 CmdAbs * CmdBoucleFor::getChild(int iChild)
 { 
-	return nullptr; // A completer
+	return m_commandes[iChild].get();
 }
 
-// Ajouter un enfant a la fin du vecteur de commandes
+/** 
+*
+*
+* Ajouter un enfant a la fin du vecteur de commandes
+*/
 void CmdBoucleFor::addChild(CmdAbs * cmd) 
 { 
-	// A completer
+	m_commandes.push_back((std::shared_ptr<CmdAbs>) cmd);
 }
